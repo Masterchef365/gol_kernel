872862dcb88914520e0e1c7a1da9b4e054f09e56
Masterchef365 <duncan.freeman1@gmail.com>
2021-09-02 01:23:50 -0700

    GOL rules

--- src/main.rs
+++ src/main.rs
@@ -46,29 +46,77 @@ fn display_gol(gol: &GolBuffer) {
     }
 }
 
- fn hello() {
-     let color: u8 = 0xb;
-     static HELLO: &[u8] = b"HELLO";
-     for (i, &b) in HELLO.iter().enumerate() {
-         let idx = 2 * i as isize;
-         unsafe {
-             *VGA_BUFFER.offset(idx) = b;
-             *VGA_BUFFER.offset(idx+1) = color;
-         }
-     }
- }
+fn in_bounds(x: isize, y: isize) -> bool {
+    x >= 0 && y >= 0 && x < WIDTH as isize && y < HEIGHT as isize
+}
+
+fn gol_index(x: isize, y: isize) -> Option<usize> {
+    in_bounds(x, y).then(|| x as usize + y as usize * WIDTH)
+}
+
+fn read_gol(buf: &GolBuffer, x: isize, y: isize) -> Option<bool> {
+    gol_index(x, y).map(|idx| buf[idx])
+}
+
+fn write_gol(buf: &mut GolBuffer, x: isize, y: isize, value: bool) {
+    if let Some(idx) = gol_index(x, y) {
+        buf[idx] = value;
+    }
+}
+
+fn read_neighbors(buf: &GolBuffer, x: isize, y: isize) -> u8 {
+    let mut n = 0;
+    for dx in -1..=1 {
+        for dy in -1..=1 {
+            if dx == 0 && dy == 0 {
+                continue;
+            }
+
+            if let Some(true) = read_gol(buf, x + dx, y + dy) {
+                n += 1;
+            }
+        }
+    }
+    n
+}
+
+fn gol_rules(neighbors: u8, cell: bool) -> bool {
+    match (neighbors, cell) {
+        (3, false) => true,
+        (2 | 3, c) => c,
+        _ => false,
+    }
+}
+
+fn step_gol(front: &mut GolBuffer, back: &GolBuffer) {
+    for x in 0..WIDTH {
+        for y in 0..HEIGHT {
+            let (x, y) = (x as isize, y as isize);
+            let neighbors = read_neighbors(back, x, y);
+            let cell = read_gol(back, x, y).unwrap();
+            let rule = gol_rules(neighbors, cell);
+            write_gol(front, x, y, rule);
+        }
+    }
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let mut color: u8 = 0;
     let mut rng = Rng::new();
 
     let mut buf_a = [false; N_CELLS];
     let mut buf_b = [false; N_CELLS];
 
+    random_gol(&mut rng, &mut buf_a);
+
+    let sleep = || for _ in 0..100_000 {};
+
     loop {
-        random_gol(&mut rng, &mut buf_a);
+        step_gol(&mut buf_b, &buf_a);
+        display_gol(&buf_b);
+        sleep();
+        step_gol(&mut buf_a, &buf_b);
         display_gol(&buf_a);
-        for i in 0..2_000_000 {}
+        sleep();
     }
 }
